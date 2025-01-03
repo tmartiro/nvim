407b95f6b887da29e10c765acdf52e9c5a512121
Tigran Martirosyan <tmartiro@gmail.com>
2025-01-03 15:11:25 +0400

    adjust theme colors

--- lua/custom/plugins/theme.lua
+++ lua/custom/plugins/theme.lua
@@ -6,10 +6,13 @@ return {
     local themeName = 'darkvoid'
     local opt = {
       colors = {
+        bg = "#112C06",
         func = "#FFFF00",
         identifier = "#F5F5DC",
         type = "#FFFF00",
-        string = "#1BFD9C"
+        string = "#1BFD9C",
+        preprocessor = "#A0B2C4",
+        comment = "#BCBCBC"
       }
     }
     local theme = require(themeName)
