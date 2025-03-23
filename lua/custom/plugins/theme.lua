112b0576fe164d0be1bced40434a56efe5063d12
Tigran Martirosyan <tmartiro@gmail.com>
2025-03-23 16:35:10 +0400

    try plan9 theme

--- lua/custom/plugins/theme.lua
+++ lua/custom/plugins/theme.lua
@@ -1,36 +1,9 @@
 return {
-  'dracula/vim',
+  "lunacookies/vim-plan9",
+  -- 'dracula/vim',
   priority = 1000,
   config = function()
-    vim.cmd.colorscheme("dracula")
+    -- vim.cmd.colorscheme("dracula")
+    vim.cmd.colorscheme("plan9")
   end,
-
-  -- "aliqyan-21/darkvoid.nvim",
-  --
-  -- priority = 1000,
-  -- init = function()
-  --   local themeName = 'darkvoid'
-  --   local opt = {
-  --     colors = {
-  --       -- bg = "#112C06",
-  --       func = "#FFFF00",
-  --       identifier = "#F5F5DC",
-  --       type = "#FFFF00",
-  --       string = "#1BFD9C",
-  --       preprocessor = "#A0B2C4",
-  --       comment = "#BCBCBC",
-  --       -- line_nr = "#BCBCBC",
-  --       -- visual = "#000000"
-  --     }
-  --   }
-  --   local theme = require(themeName)
-  --   theme.setup(opt)
-  --   vim.cmd.colorscheme(themeName)
-  -- end,
-  --
-  -- "rakr/vim-one",
-  -- priority = 1000,
-  -- config = function()
-  --   vim.cmd.colorscheme("one")
-  -- end,
 }
